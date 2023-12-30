776db3c7c1059fe91004b65359e9ac0da15e3f96
Dan Chiarlone <danilochiarlone@gmail.com>
2023-12-29 23:44:32 -0800

    Fixes pidgtm_compile_times bug (#60)
    * Fixes pidgtm_compile_times bug
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>
    
    * fix && fmt
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>
    
    * ctrlc bug
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>
    
    ---------
    
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- database/migrations/2023-12-29-222652_migration5/down.sql
+++ database/migrations/2023-12-29-222652_migration5/down.sql
@@ -1,8 +1,7 @@
--- Commands to drop the new table for error logs
 DROP TABLE IF EXISTS error_logs;
 
--- Commands to revert the repurposed table to its original form
-ALTER TABLE pidgtm_compile_times DROP COLUMN calculation_timestamp;
-ALTER TABLE pidgtm_compile_times ALTER COLUMN time_in_seconds SET DATA TYPE INTEGER;
-ALTER TABLE pidgtm_compile_times RENAME COLUMN time_in_seconds TO player_id;
-ALTER TABLE pidgtm_compile_times RENAME TO last_checked_player_id;
+DROP TABLE IF EXISTS pidgtm_compile_times;
+
+CREATE TABLE last_checked_player_id (
+    player_id INTEGER PRIMARY KEY
+);
