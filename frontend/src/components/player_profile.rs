08dff4a607b084fb472a001020503316b17d99e8
danbugs <danilochiarlone@gmail.com>
2024-01-01 16:14:09 -0800

    Make Smithe Multi-Page
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- frontend/src/components/player_profile.rs
+++ frontend/src/components/player_profile.rs
@@ -1,4 +1,5 @@
-use yew::{function_component, html, Html, Properties};
+use gloo_net::http::Request;
+use yew::{function_component, html, use_effect_with, use_state, Html, Properties};
 
 use crate::models::{Player, Set, Tournament};
 
@@ -10,52 +11,171 @@ use crate::components::player_profile::player_profile_tournament_list::PlayerPro
 
 pub mod player_profile_summary_data;
 use crate::components::player_profile::player_profile_summary_data::PlayerProfileSummaryData;
+use crate::utils::parse_text_vector;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
-    pub display: bool,
-    pub selected_player: Option<Player>,
-    pub selected_player_summary_data: Option<(String, String, String, String, Vec<String>)>,
-    pub selected_player_tournaments: Option<Vec<Tournament>>,
-    pub selected_tournament_sets: Option<Vec<Set>>,
+    pub player_id: i32,
 }
 
 #[function_component(PlayerProfile)]
 pub fn player_profile(props: &Props) -> Html {
-    if props.display {
-        let selected_player = props.selected_player.clone().unwrap();
-        html! {
-            <div>
+    let pid = props.player_id;
+    let selected_player = use_state(|| None::<Player>);
+    let selected_player_summary_data =
+        use_state(|| None::<(String, String, String, String, Vec<String>)>);
+    let selected_player_tournaments = use_state(|| None::<Vec<Tournament>>);
+    let selected_tournament_sets = use_state(|| None::<Vec<Set>>);
+    {
+        let selected_player = selected_player.clone();
+        let selected_player_summary_data = selected_player_summary_data.clone();
+        let selected_player_tournaments = selected_player_tournaments.clone();
+        let selected_tournament_sets = selected_tournament_sets.clone();
+
+        use_effect_with((), move |_| {
+            let selected_player = selected_player.clone();
+            let selected_player_summary_data = selected_player_summary_data.clone();
+            let selected_player_tournaments = selected_player_tournaments.clone();
+            let selected_tournament_sets = selected_tournament_sets.clone();
+
+            wasm_bindgen_futures::spawn_local(async move {
+                // get player
+                let fetched_player: Player =
+                    Request::get(&format!("{}/player/{}", env!("SERVER_ADDRESS"), pid))
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+
+                selected_player.set(Some(fetched_player));
+
+                // get tournaments, and sets
+                let fetched_tournaments: Vec<Tournament> =
+                    Request::get(&format!("{}/tournaments/{}", env!("SERVER_ADDRESS"), pid))
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+
+                let fetched_sets: Vec<Set> =
+                    Request::get(&format!("{}/sets/{}", env!("SERVER_ADDRESS"), pid))
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+
+                let num_tournaments = fetched_tournaments.len();
+
+                selected_player_tournaments.set(Some(fetched_tournaments));
+                selected_tournament_sets.set(Some(fetched_sets));
+
+                // get summary data
+                let fetch_winrate: String =
+                    Request::get(&format!("{}/sets/{}/winrate", env!("SERVER_ADDRESS"), pid))
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+
+                let fetch_competitor_type: String = Request::get(&format!(
+                    "{}/sets/{}/competitor_type",
+                    env!("SERVER_ADDRESS"),
+                    pid
+                ))
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+
+                let fetch_wins_without_dqs: String = Request::get(&format!(
+                    "{}/sets/{}/wins_without_dqs",
+                    env!("SERVER_ADDRESS"),
+                    pid
+                ))
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+
+                let fetch_losses_without_dqs: String = Request::get(&format!(
+                    "{}/sets/{}/losses_without_dqs",
+                    env!("SERVER_ADDRESS"),
+                    pid
+                ))
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+
+                let fetch_top_two_characters: String = Request::get(&format!(
+                    "{}/player/{}/top_two_characters",
+                    env!("SERVER_ADDRESS"),
+                    pid
+                ))
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+
+                let wins_losses =
+                    format!("{}-{}", fetch_wins_without_dqs, fetch_losses_without_dqs);
+                let tournaments_attended = format!("{}", num_tournaments);
+
+
+                selected_player_summary_data.set(Some((
+                    fetch_winrate,
+                    fetch_competitor_type,
+                    wins_losses,
+                    tournaments_attended,
+                    parse_text_vector(&fetch_top_two_characters),
+                )));
+            });
+        });
+    }
+
+    html! {
+        <div>
             <br/>
             <div class="col-md-12 mb-5" style="background-color:#F3F4F6">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
                             <PlayerProfileHeader
-                                selected_player={selected_player.clone()}
-                                display={props.selected_player_summary_data.is_some()}
-                                selected_player_summary_data={props.selected_player_summary_data.clone()}
+                                selected_player={(*selected_player).clone()}
+                                display={(*selected_player_summary_data).is_some()}
+                                selected_player_summary_data={(*selected_player_summary_data).clone()}
                             />
                             <hr/>
                             <PlayerProfileSummaryData
-                                display={props.selected_player_summary_data.is_some()}
-                                selected_player_summary_data={props.selected_player_summary_data.clone()}
+                                display={(*selected_player_summary_data).is_some()}
+                                selected_player_summary_data={(*selected_player_summary_data).clone()}
                             />
                             <hr/>
                             <PlayerProfileTournamentList
-                                display={props.selected_player_tournaments.is_some()}
-                                selected_player_tournaments={props.selected_player_tournaments.clone()}
-                                selected_tournament_sets={props.selected_tournament_sets.clone()}
+                                display={(*selected_player_tournaments).is_some()}
+                                selected_player_tournaments={(*selected_player_tournaments).clone()}
+                                selected_tournament_sets={(*selected_tournament_sets).clone()}
                             />
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
-        }
-    } else {
-        html! {
-            <div></div>
-        }
+        </div>
     }
 }
