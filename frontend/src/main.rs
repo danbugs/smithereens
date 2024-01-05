3a7c1c72d0b970d545569544f868b8b408f6ec00
danbugs <danilochiarlone@gmail.com>
2024-01-05 13:41:14 -0800

    updated readme and update home page
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -43,11 +43,33 @@ fn switch(routes: Route) -> Html {
             {
                 match routes {
                     Route::Home => html! {
-                    <div class="text-center" style="color:#C6263E">
-                        <br/>
-                        <br/>
-                        <h2>{"• • •"}</h2>
-                    </div>
+                        <div class="why-join-section my-5">
+                            <div class="row align-items-start">
+                                <div class="col-md-6">
+                                    <h3 class="mb-3">{"Why Smithereens?"}</h3>
+                                    <p class="lead">{"Accessible anywhere, Smithereens is the all-in-one tool for Smash analytics — whether you're a fan or a fierce competitor."}</p>
+                                    <ul class="list-unstyled mb-4">
+                                        <li><i class="bi bi-check2"></i>{" CLI & Web App"}</li>
+                                        <li><i class="bi bi-check2"></i>{" Fully Open-Source"}</li>
+                                        <li><i class="bi bi-check2"></i>{" Independent & Community-Driven"}</li>
+                                        <li><i class="bi bi-check2"></i>{" Continuously Tested & Reliable"}</li>
+                                    </ul>
+                                    <p class="mt-4 mb-2">{"Experience unique features:"}</p>
+                                    <ul class="list-unstyled">
+                                        <li><i class="bi bi-twitter"></i>{" Twitter Integration"}</li>
+                                        <li><i class="bi bi-graph-up"></i>{" Insightful Metrics"}</li>
+                                        <li><i class="bi bi-plus-circle"></i>{" And much more..."}</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="discord-invite text-center py-5 px-4" style="background-color: #F8F9FA;">
+                                        <h3 class="mb-3">{"Join Our Discord"}</h3>
+                                        <p class="mb-4">{"Got suggestions or found a bug? We're all ears on Discord. Join our community and help shape Smithereens!"}</p>
+                                        <a href="https://discord.gg/X935GRyyWE" class="btn btn-primary btn-lg" target="_blank">{"Join Discord"}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     },
                     Route::PlayerList { gamer_tag } => html! {
                         <PlayerList
